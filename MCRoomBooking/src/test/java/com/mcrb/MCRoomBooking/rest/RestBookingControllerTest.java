ba2e54346242d52bd212bd528f304bc0665d00ab
e092344 <swadeep.mohanty@mastercard.com>
2020-02-17 16:31:28 +0530

    New Test cases added

--- MCRoomBooking/src/test/java/com/mcrb/MCRoomBooking/rest/RestBookingControllerTest.java
+++ MCRoomBooking/src/test/java/com/mcrb/MCRoomBooking/rest/RestBookingControllerTest.java
@@ -3,6 +3,7 @@ package com.mcrb.MCRoomBooking.rest;
 import com.mcrb.MCRoomBooking.data.BookingRepository;
 import com.mcrb.MCRoomBooking.model.Layout;
 import com.mcrb.MCRoomBooking.model.entities.Booking;
+import com.mcrb.MCRoomBooking.model.entities.LayoutCapacity;
 import com.mcrb.MCRoomBooking.model.entities.Room;
 import com.mcrb.MCRoomBooking.model.entities.User;
 import org.assertj.core.api.Assertions;
@@ -29,11 +30,12 @@ public class RestBookingControllerTest {
 	BookingRepository bookingRepository;
 
 	@Test
-	public void getBooking_whenIdPassed(){
+	public void getAllBookings_whenDatePassed(){
 		//Given
 		Room room = new Room("Abdul Kalam","4th Floor");
 		User user = new User("John","");
-		Booking booking = new Booking(room, user, Layout.BOARD,"Test Meeting",new Date(1581877800000L), new Time(19800000),new Time(21600000), 10);
+		Booking booking = new Booking(room, user, Layout.BOARD,"Test Meeting",new Date(1581877800000L),
+				new Time(19800000),new Time(21600000), 10);
 		List<Booking> bookings = new ArrayList<>();
 		bookings.add(booking);
 		Date sqlDate = Date.valueOf("2020-02-17");
@@ -45,5 +47,25 @@ public class RestBookingControllerTest {
 		//then
 		Assertions.assertThat(bookings1.size()).isEqualTo(1);
 
+	}
+
+	@Test
+	public void getConfirmation_whenNewBooking(){
+		//Given
+		Room room = new Room("Abdul Kalam","4th Floor");
+		LayoutCapacity layoutCapacity = new LayoutCapacity(Layout.THEATER,50);
+		List<LayoutCapacity> layoutCapacities = new ArrayList<>();
+		layoutCapacities.add(layoutCapacity);
+		room.setCapacities(layoutCapacities);
+		User user = new User("John","");
+
+		Booking booking = new Booking(room, user, Layout.THEATER,"Test Meeting",new Date(1581877800000L),
+				new Time(19800000),new Time(21600000), 10);
+
+		//when
+		Booking booking1 = restBookingsController.newBooking(booking);
+
+		//then
+		Assertions.assertThat(booking1.getTitle()).isEqualTo("Test Meeting");
 	}
 }
